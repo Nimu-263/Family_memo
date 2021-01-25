336b8898b63cd1847cb9330a6258a5f750c9bc89
Nimu-263 <junnishimura26@gmail.com>
2021-01-25 17:47:32 +0900

    既読機能のサーバーサイド実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,19 @@ class HomesController < ApplicationController
   def index
     @boards = Board.all.order(id: "DESC")
   end
+
+  def create
+  end
+
+  def checked
+    post = Board.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    memo = Board.find(params[:id])
+    render json: { post: memo }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "homes#index"
   resources :homes, only: [:index, :create, :destroy]
+  get 'homes/:id', to: 'homes#checked'
 end
