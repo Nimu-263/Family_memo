985874b9aea4033200504497347165987e5e54f2
Nimu-263 <junnishimura26@gmail.com>
2021-02-06 14:40:08 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:edit, :update]
   root to: "homes#index"
   resources :homes, only: [:index, :create, :destroy]
   get 'homes/:id', to: 'homes#checked'
