83205578410cb83d140ff5180797533b4cbdbfa2
Nimu-263 <junnishimura26@gmail.com>
2021-02-13 16:25:39 +0900

    不要コードを削除

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,4 @@
 class MemosController < ApplicationController
-  before_action :move_to_registration
 
   def create
     memo = Memo.create(memo_params)
@@ -22,12 +21,6 @@ class MemosController < ApplicationController
 
   def memo_params
     params.permit(:title, :content).merge(user_id: current_user.id, checked: false)
-  end
-
-  def move_to_registration
-    unless user_signed_in?
-      redirect_to new_user_registration_path
-    end
   end
 
 end
