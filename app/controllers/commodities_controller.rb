e301722c339fb8b918b192cc1eec2cff3e35dc48
Nimu-263 <junnishimura26@gmail.com>
2021-02-20 13:29:56 +0900

    commodities_controllerにcreateアクションを追加

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,9 +1,32 @@
 class CommoditiesController < ApplicationController
+  before_action :move_to_registration
+
   def index
     @commodities = Commodity.all.order(id: "DESC")
   end
 
   def new
     @commodity = Commodity.new
+  end
+
+  def create
+    @commodity = Commodity.new(commodity_params)
+    if @commodity.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def commodity_params
+    params.require(:commodity).permit(:image, :name, :price, :quantity, :storage_location, :purchase_shop, :purchase_day, :explanation).merge(user_id: current_user.id)
+  end
+
+  def move_to_registration
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
